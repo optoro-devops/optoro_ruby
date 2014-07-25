f0f09f8394a4572bc97cbdfb11ac39c82781229d
Jeff Gillis <jgillis@optoro.com>
2014-07-25 15:43:36 -0400

    DO-190: Update version for ruby cookbook.

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'jgillis@optoro.com'
 license          'All rights reserved'
 description      'Installs/Configures optoro_ruby'
 long_description 'Installs/Configures optoro_ruby'
-version          '0.1.3'
+version          '0.1.4'
 
 depends "apt"
 depends "build-essential"
