05b4a7d8d2973f0610bfe0fa998c4d339f19ec1d
Jeff Gillis <jgillis@optoro.com>
2014-09-04 12:18:33 -0400

    Update build source recipe to use lazy parameter evaluation.

--- recipes/build_source.rb
+++ recipes/build_source.rb
@@ -2,9 +2,14 @@
 # Description: Install ruby from source
 
 # Compile and install ruby
+major_version = node[:optoro_ruby][:ruby_major_version]
+minor_version = node[:optoro_ruby][:ruby_minor_version]
+params = { major: major_version, minor: minor_version }
+source_directory = node[:optoro_ruby][:source_directory] % params
+
 execute "install_ruby" do
   command "./configure && make && make install"
-  cwd node[:optoro_ruby][:source_directory]
+  cwd source_directory
   not_if { File.exist?("/usr/local/bin/ruby") }
   subscribes :run, "execute[extract_ruby]", :immediately
   action :nothing
