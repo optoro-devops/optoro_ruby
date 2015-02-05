e72ac4f1ffda2242fdb4c26dd9babb77df61ae2a
Jeff Gillis <jgillis@optoro.com>
2015-02-05 14:29:36 -0500

    Bump Version

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer 'devops@optoro.com'
 license 'MIT'
 description 'This cookbook installs ruby'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '0.0.1'
+version '1.0.0'
 
 depends 'apt'
 depends 'build-essential'
