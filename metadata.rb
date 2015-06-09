b427477327512897d8faac64251765efbb0c19d9
Jeff Gillis <jgillis@optoro.com>
2015-06-09 10:07:50 -0400

    Add overcommit
    Bump version

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer 'devops@optoro.com'
 license 'MIT'
 description 'This cookbook installs ruby'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '1.0.3'
+version '1.0.4'
 
 depends 'apt'
 depends 'build-essential'
